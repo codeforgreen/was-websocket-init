0cc4179f290bb8c0d5dec431473728abf5a75018
vborcea <virgil.borcea@esolutions.ro>
2017-07-09 16:39:45 +0300

    * adding public message

--- src/main/java/ro/vborcea/was/ws/Application.java
+++ src/main/java/ro/vborcea/was/ws/Application.java
@@ -10,6 +10,7 @@ import java.util.Map;
 public class Application {
     static final Map<String,String> WS_MAPPING = new HashMap<>();
 
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
